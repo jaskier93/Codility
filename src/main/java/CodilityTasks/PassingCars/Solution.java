7d27dc88733fdb41a7205889a0401de1f11cb729
Damian Grochowski <smg7@o2.pl>
2019-09-28 22:32:05 +0200

    passingCars, TODO: optimize algorithm

---
+++ src/main/java/CodilityTasks/PassingCars/Solution.java
@@ -0,0 +1,29 @@
+package CodilityTasks.PassingCars;
+
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] A) {
+
+        //TODO: zoptymalizować algorytm, podwójna pętla wykonuje się zbyt długo (złożoność O(N^2)) dla tablic o dużych rozmiarach (A.length>1000)
+
+        if (Arrays.stream(A).distinct().count() == 1) {
+            return 0;
+        }
+        int score = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == 0) {
+                for (int j = 1; j < A.length; j++) {
+                    if (A[j] == 1 && j > i) {
+                        score++;
+                    }
+                }
+            }
+        }
+        if (score < 1_000_001) {
+            return score;
+        }
+        return -1;
+    }
+}

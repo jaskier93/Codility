f53326170876d665eed2db043c80b5be5def5bb9
Damian Grochowski <smg7@o2.pl>
2019-10-04 23:54:59 +0200

    more efficient way to solve PassingCars -> 100%

--- src/main/java/CodilityTasks/PassingCars/Solution.java
+++ src/main/java/CodilityTasks/PassingCars/Solution.java
@@ -26,4 +26,20 @@ class Solution {
         }
         return -1;
     }
+
+    // codility score ->100%
+    public int moreEfficientSolution(int[] A) {
+        int zerosCounter = 0;
+        int score = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == 0)
+                zerosCounter++;
+            if (A[i] == 1)
+                score = score + zerosCounter;
+            if (score > 1000000000)
+                return -1;
+        }
+        return score;
+    }
 }

93ec1437a095661f6881db313ff09c1c7c2f552f
Damian Grochowski <smg7@o2.pl>
2019-10-05 22:27:04 +0200

    Cycling Rotation - > fixed if -> 100% score

--- src/main/java/CodilityTasks/CyclingRotation/Solution.java
+++ src/main/java/CodilityTasks/CyclingRotation/Solution.java
@@ -1,52 +1,24 @@
 package CodilityTasks.CyclingRotation;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 class Solution {
     public int[] solution(int[] A, int K) {
 
-        if (K > A.length) {
+        if (K > A.length && A.length > 1)
             K = K % A.length;
-        }
 
         long distinctElements = Arrays.stream(A)
                 .distinct()
                 .count();
 
-        if (A == null || K == 0 || A.length > 100 || K > 100) {
-            return null;
-        } else if (K == A.length || K == 1 || distinctElements == 1) {
+        if (A.length == 0 || K == 0 || K == A.length || distinctElements == 1)
             return A;
-        } else {
-            List<Integer> arrayList = Arrays.stream(A)
-                    .boxed()
-                    .filter(c -> (c > -1001 && c < 1001))
-                    .collect(Collectors.toList());
 
-            List<Integer> rotatedList = new ArrayList<>();
-
-            rotatedList.addAll(arrayList.subList(K - 1, arrayList.size()));
-            rotatedList.addAll(arrayList.subList(0, K - 1));
-
-            int[] rotatedArray = new int[rotatedList.size()];
-
-          //  Collections.rotate(arrayList, K);
-
-            for (int i = 0; i < rotatedList.size(); i++) {
-                rotatedArray[i] = rotatedList.get(i);
-            }
-
-            return rotatedArray;
+        int[] rotatedArray = new int[A.length];
+        for (int i = 0; i < A.length; i++) {
+            rotatedArray[(i + K) % A.length] = A[i];
         }
+        return rotatedArray;
     }
-/*
-    public static void main(String[] args) {
-        int[] array = {3, 8, 9, 7, 6};
-        Solution solution = new Solution();
-        solution.solution(array, 3);
-    }*/
 }

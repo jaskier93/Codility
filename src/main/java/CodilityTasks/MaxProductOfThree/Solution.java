b5e1001ae0e945e44e00d84926c0a8c16c69dd08
Damian Grochowski <smg7@o2.pl>
2019-10-22 00:42:26 +0200

    MaxProductOfThree ->100%

--- src/main/java/CodilityTasks/MaxProductOfThree/Solution.java
+++ src/main/java/CodilityTasks/MaxProductOfThree/Solution.java
@@ -3,16 +3,18 @@ package CodilityTasks.MaxProductOfThree;
 import java.util.Arrays;
 
 class Solution {
-    public int solution(int[] A) {
-
+    public static int solution(int[] A) {
+        int max, max2 = 0;
         Arrays.sort(A);
-        int max = A[A.length - 1] * A[A.length - 2] * A[A.length - 3];
-        if (A[0] < 0 && A[1] < 0 && Math.abs(A[0]) + Math.abs(A[1]) > A[A.length - 1] + A[A.length - 2]) {
-            max = A[0] * A[1] * A[A.length - 1];
+        max = A[A.length - 1] * A[A.length - 2] * A[A.length - 3];
+        if (A[0] < 0 && A[1] < 0) {
+            max2 = A[0] * A[1] * A[A.length - 1];
         }
         if (A[A.length - 1] < 0) {
             max = A[A.length - 1] * A[A.length - 2] * A[A.length - 3];
         }
+        if (max2 > max)
+            return max2;
         return max;
     }
 }
